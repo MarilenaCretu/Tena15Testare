550cb11b4b7a59c4181f1ae6509926a8e56112cd
MarilenaCretu <marilenac2001@gmail.com>
2022-11-28 00:20:26 +0200

    Commit

--- src/main/java/TemaDoi/Main.java
+++ src/main/java/TemaDoi/Main.java
@@ -1,7 +1,14 @@
 package TemaDoi;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Read read=new Read();
+        read.getOricenumber();
 
     }
+
 }
+
--- src/main/java/TemaDoi/Read.java
+++ src/main/java/TemaDoi/Read.java
@@ -4,6 +4,26 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Read {
-    private static void user3enteries() {
+    public static int getOricenumber() {
+        Scanner scan = new Scanner(System.in);
+        int number = 0;
+        int[] myArray = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        boolean repeat = true;
+        while (repeat == true) {
+            try {
+                System.out.print("Please enter a number: ");
+                number = scan.nextInt();
+                repeat = false;
+                System.out.println(myArray[number]);
+            } catch (ArrayIndexOutOfBoundsException e) {
+                repeat = true;
+                System.out.println("Numarul nu se afla in intervalul [0 - " + (myArray.length - 1) + "]");
+            } catch (InputMismatchException e) {
+                System.out.println("The value [" + scan.nextLine() + "] is not a numeric value.\nPlease try again!");
+            }
+        }
+        System.out.println("cc"+number+"valid");
+        return number;
     }
+
 }
