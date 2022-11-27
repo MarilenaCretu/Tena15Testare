a91cdbe8d57eff4cf5f7aba90e1b2eafa2774236
MarilenaCretu <marilenac2001@gmail.com>
2022-11-28 00:01:34 +0200

    Commit

---
+++ src/main/java/TemaDoi/Main.java
@@ -0,0 +1,7 @@
+package TemaDoi;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/TemaDoi/Read.java
@@ -0,0 +1,9 @@
+package TemaDoi;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Read {
+    private static void user3enteries() {
+    }
+}
